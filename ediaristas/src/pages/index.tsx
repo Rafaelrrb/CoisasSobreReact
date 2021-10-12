b608e2736afd5048e0a4ae84e6b51bb17cf0944d
Rafael <rafaeborba@gmail.com>
2021-10-11 23:09:59 -0300

    att 1 commit

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -2,8 +2,28 @@ import type { NextPage } from "next";
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data display/PageTitle/PageTitle";
 import Userinformation from "ui/components/data display/Userinformation/Userinformation";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
+import { Button, Typography, Container, CircularProgress } from "@mui/material";
+import {
+  FormeElementsContainer,
+  ProfissionaisPaper,
+  ProfissionaisContainer,
+} from "ui/styles/pages/index.style";
+import useIndex from "data/hooks/pages/useIndex.page";
 
 const Home: NextPage = () => {
+  const {
+    cep,
+    setCep,
+    cepValido,
+    buscarProfissionais,
+    erro,
+    diaristas,
+    buscaFeita,
+    carregando,
+    diaristasRestantes,
+  } = useIndex();
+
   return (
     <div>
       <SafeEnvironment />
@@ -14,12 +34,73 @@ const Home: NextPage = () => {
         }
       />
 
-      <Userinformation
-        nome={"Rafa"}
-        picture={"https://github.com/Rafaelrrb.png"}
-        rating={3}
-        description={"SLS"}
-      />
+      <Container>
+        <FormeElementsContainer>
+          <TextFieldMask
+            mask={"99.999-999"}
+            label={"Digite seu CEP"}
+            fullWidth
+            variant={"outlined"}
+            value={cep}
+            onChange={(event) => setCep(event.target.value)}
+          />
+
+          {erro && <Typography color={"error"}> {erro} </Typography>}
+
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            sx={{ width: "220px" }}
+            disabled={!cepValido || carregando}
+            onClick={() => buscarProfissionais(cep)}
+          >
+            {carregando ? <CircularProgress size={20} /> : "Buscar"}
+          </Button>
+        </FormeElementsContainer>
+
+        {buscaFeita &&
+          (diaristas.length > 0 ? (
+            <ProfissionaisPaper>
+              <ProfissionaisContainer>
+                {diaristas.map((item, index) => {
+                  return (
+                    <Userinformation
+                      key={index}
+                      nome={item.nome_completo}
+                      picture={item.foto_usuario}
+                      rating={item.reputacao}
+                      description={item.cidade}
+                    />
+                  );
+                })}
+              </ProfissionaisContainer>
+              <Container sx={{ textAlign: "center" }}>
+                {diaristasRestantes > 0 && (
+                  <Typography sx={{ mt: 5 }}>
+                    ... e mais {diaristasRestantes}{" "}
+                    {diaristasRestantes > 1
+                      ? "profissionais atemdem"
+                      : "profissional atende"}{" "}
+                    ao seu endereço
+                  </Typography>
+                )}
+
+                <Button
+                  variant={"contained"}
+                  color={"secondary"}
+                  sx={{ mt: 5 }}
+                >
+                  {" "}
+                  Contratar um profissional{" "}
+                </Button>
+              </Container>
+            </ProfissionaisPaper>
+          ) : (
+            <Typography align={"center"} color={"textPrimary"}>
+              Ainda não temos nenhuma diarista disponivel sem sua região
+            </Typography>
+          ))}
+      </Container>
     </div>
   );
 };
---
+++ ediaristas/src/ui/components/surfaces/Footer/Footer.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  FooterStyled,
+  FooterContainer,
+  FooterTitle,
+  AppList,
+} from "./Footer.style";
+import { Typography, Box } from "@mui/material";
+
+const Footer = () => {
+  return (
+    <FooterStyled>
+      <FooterContainer>
+        <Box sx={{ maxWidth: "400px" }}>
+          <FooterTitle> Quem somos</FooterTitle>
+          <Typography variant={"body2"} sx={{ mt: 2 }}>
+            {" "}
+            alguma coisa{" "}
+          </Typography>
+        </Box>
+
+        <div>
+          <FooterTitle> Baixe nossos aplicativos </FooterTitle>
+          <AppList>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
+                <img src={"/img/logos/app-store.png"} alt={"App Store"} />
+              </a>
+            </li>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
+                <img src={"/img/logos/google-play.png"} alt={"Google Play"} />
+              </a>
+            </li>
+          </AppList>
+        </div>
+      </FooterContainer>
+    </FooterStyled>
+  );
+};
+
+export default Footer;
